8a8c623db63e28713323457c219262884b413f8b
OtohaTanaka <otohaoppy@gmail.com>
2021-05-12 23:27:46 +0900

    Initial Commit

---
+++ WordList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordList
+//
+//  Created by 田中音羽 on 2021/05/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
